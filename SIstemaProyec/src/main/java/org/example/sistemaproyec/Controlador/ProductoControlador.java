333b27cc94e23e6c281ef7f28a6bf54ae87e4f01
Jenrri Puch <jenrripuchdzul@gmail.com>
2024-11-08 15:46:32 -0600

    añadi eliminacionProducto y Editar producto checar porfavor

--- SIstemaProyec/src/main/java/org/example/sistemaproyec/Controlador/ProductoControlador.java
+++ SIstemaProyec/src/main/java/org/example/sistemaproyec/Controlador/ProductoControlador.java
@@ -35,7 +35,54 @@ public class ProductoControlador {
     }
 
     public void EditarProducto() {
+        System.out.println("Editar Producto");
+        System.out.println("Ingresar el numero de producto que deseas editar: ");
+        Scanner sd = new Scanner(System.in);
+        int p=sd.nextInt();
 
+        System.out.println("¿Que parte desea modificar el producto? ");
+        System.out.println("1.Nombre ");
+        System.out.println("2.Descripcion ");
+        System.out.println("3.Precio ");
+        System.out.println("4.CantidadDisponible ");
+        Scanner sa = new Scanner(System.in);
+        int c = sa.nextInt();
+
+        switch(c){
+            case 1:
+                System.out.println("Ingresar el nuevo nombre del producto: ");
+                Scanner sc = new Scanner(System.in);
+                String n = sc.nextLine();
+                Producto[p].setNombre(n);
+                break;
+            case 2:
+                System.out.println("Ingresar  la nueva descripcion del producto: ");
+                Scanner sc1 = new Scanner(System.in);
+                String d = sc1.nextLine();
+                Producto[p].setDescripcion(d);
+                break;
+            case 3:
+                System.out.println("Ingresar el nuevo precio del producto: ");
+                Scanner sc2 = new Scanner(System.in);
+                double pr=sc2.nextDouble();
+                Producto[p].setPrecio(pr);
+                break;
+            case 4:
+                System.out.println("Ingresar el nuevo cantidad disponible del producto: ");
+                Scanner sc3 = new Scanner(System.in);
+                int cd=sc3.nextInt();
+                Producto[p].setCantidadDisponible(cd);
+                break;
+
+        }
+    }
+
+    public void EliminarProducto() {
+        System.out.println("Eliminar Producto");
+        System.out.println("Ingresar el nuevo nombre del producto que quiere eliminar: ");
+        Scanner se = new Scanner(System.in);
+        int e=se.nextInt();
+        Producto[e]=null;
     }
 
 }
