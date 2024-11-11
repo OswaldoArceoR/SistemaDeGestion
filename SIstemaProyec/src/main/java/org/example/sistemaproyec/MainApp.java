83527f94492e4f61b8c4290c4a303ae8643fd99a
Daniel Bacelis <jesus.bacelis.santos@gmail.com>
2024-11-11 10:00:24 -0600

    Pantalla de inicio

--- SIstemaProyec/src/main/java/org/example/sistemaproyec/MainApp.java
+++ SIstemaProyec/src/main/java/org/example/sistemaproyec/MainApp.java
@@ -12,12 +12,11 @@ public class MainApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/main/resources/org/example/sistemaproyec/Vista/ProductoVista.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 300);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
         primaryStage.setTitle("Sistema de Gestión de Materiales - Productos");
         primaryStage.setScene(scene);
         primaryStage.show();
         primaryStage.getIcons().add(new Image(getClass().getResource("/main/resources/org/example/sistemaproyec/un_lobo_ingeniero.png").toExternalForm()));
-
     }
 
     public static void main(String[] args) {
