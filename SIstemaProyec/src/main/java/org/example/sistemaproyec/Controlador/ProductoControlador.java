9573bd110f9dacc17ef0ff35bfe00a41a66c183d
Jenrri Puch <jenrripuchdzul@gmail.com>
2024-11-27 09:25:11 -0600

    Se agrego al controlador la clasificacion a editar agregar, y eliminar

--- SIstemaProyec/src/main/java/org/example/sistemaproyec/Controlador/ProductoControlador.java
+++ SIstemaProyec/src/main/java/org/example/sistemaproyec/Controlador/ProductoControlador.java
@@ -10,20 +10,22 @@ public class ProductoControlador {
 
     private List<Producto> productos = new ArrayList<>();
 
-    public void agregarProducto(String nombre, String descripcion, double precio, int cantidadDisponible) throws ProductoException {
+    public void agregarProducto(String nombre, String descripcion,String clasificacion, double precio, int cantidadDisponible) throws ProductoException {
         Producto producto = new Producto();
         producto.setNombre(nombre);
         producto.setDescripcion(descripcion);
+        producto.setClasificacion(clasificacion);
         producto.setPrecio(precio);
         producto.setCantidadDisponible(cantidadDisponible);
         productos.add(producto);
     }
 
-    public void editarProducto(int index, String nombre, String descripcion, double precio, int cantidadDisponible) {
+    public void editarProducto(int index, String nombre, String descripcion, String clasificacion, double precio, int cantidadDisponible) {
         if (index >= 0 && index < productos.size()) {
             Producto producto = productos.get(index);
             producto.setNombre(nombre);
             producto.setDescripcion(descripcion);
+            producto.setClasificacion(clasificacion);
             producto.setPrecio(precio);
             producto.setCantidadDisponible(cantidadDisponible);
         }
